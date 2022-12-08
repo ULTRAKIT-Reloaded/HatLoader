d67b4d00a16d642fa3dbe5cd3da9d3d4cf9aa039
PetersonE1 <PetersonE_1@live.com>
2022-12-07 19:03:30 -0500

    first build

---
+++ HatLoader/CommandInjector.cs
@@ -0,0 +1,43 @@
+﻿using GameConsole;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ULTRAKIT.Data;
+
+namespace HatLoader
+{
+    public class SetHatState : ICommand
+    {
+        public string Name => "Set Hat State";
+        public string Description => "Sets the state of a custom hat.";
+        public string Command => "hatstate";
+
+        public void Execute(GameConsole.Console con, string[] args)
+        {
+            if (args.Length < 2)
+            {
+                con.PrintLine("Usage: hatstate <hatID> <active>");
+                return;
+            }
+            ULTRAKIT.Loader.HatLoader.SetAllActive(args[0], bool.Parse(args[1]));
+        }
+    }
+
+    public class GetHatIDs : ICommand
+    {
+        public string Name => "Get Hats";
+        public string Description => "Lists all hat IDs.";
+        public string Command => "gethats";
+
+        public void Execute(GameConsole.Console con, string[] args)
+        {
+            foreach (HatRegistry hat in ULTRAKIT.Loader.HatLoader.registries)
+            {
+                con.PrintLine(hat.hatID);
+            }
+            ULTRAKIT.Loader.HatLoader.SetAllActive(args[0], bool.Parse(args[1]));
+        }
+    }
+}
---
+++ HatLoader/Main.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UMM;
+using ULTRAKIT;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System.IO;
+using ULTRAKIT.Extensions;
+
+namespace HatLoader
+{
+    [UKPlugin("Hat Loader", "1.0.0", "Loads custom hat bundles using ULTRAKIT Reloaded", false, false)]
+    public class Plugin : UKMod
+    {
+        public override void OnModLoaded()
+        {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+            List<AssetBundle> bundles = new List<AssetBundle>();
+
+            string dir = $@"{modFolder}\HatBundles";
+            if (!Directory.Exists(dir))
+                Directory.CreateDirectory(dir);
+
+            foreach(string file in Directory.EnumerateFiles(dir))
+            {
+                try
+                {
+                    bundles.Add(AssetBundle.LoadFromFile(file));
+                }
+                catch
+                {
+                    Debug.LogWarning($"Failed to load: {file}");
+                }
+            }
+
+            foreach (AssetBundle bundle in bundles)
+            {
+                ULTRAKIT.Loader.HatLoader.LoadHats(bundle);
+            }
+        }
+
+        public override void OnModUnload()
+        {
+            PlayerPrefs.SetInt("CurSlo", 1);
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            GameConsole.Console.Instance.RegisterCommand(new SetHatState());
+            GameConsole.Console.Instance.RegisterCommand(new GetHatIDs());
+        }
+    }
+}
